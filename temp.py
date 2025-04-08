a44e8db33cf00962c7052948e5d5a4381d589b8e
Katia <katia.punter@storyshareplatform.com>
2025-04-08 15:15:21 +0100

    adds failing test

---
+++ temp.py
@@ -0,0 +1,2 @@
+def celsius_to_far(celsius_temp):
+    pass
--- test_temp.py
+++ test_temp.py
@@ -1,8 +1,12 @@
 import unittest
+from temp import celsius_to_far
 
 class TestTemp(unittest.TestCase):
     def test_tests_work(self):
         self.assertEqual(2+2,4)
+
+    def test_celsius_to_far_happy(self):
+        self.assertEqual(celsius_to_far(0), 32)
 
 if __name__ == "__main__":
     unittest.main()
